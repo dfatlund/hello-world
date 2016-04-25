8d3c4f14330f2a52680c43ff0aff10d4060fd818
Dan <dfatlund@mac.com>
2016-04-24 18:13:31 -0700

    some bs

--- hworld.py
+++ hworld.py
@@ -1,5 +1,5 @@
 print "Hello world of GIThub"
 ttt=23
-print "Hello " + str(ttt*2)
-print "this is a public repository"
+print "Hello " + str(ttt*2+55)
+print "this is a public repository but I have a private too"
 print 'now this file has been changed from windows too'
